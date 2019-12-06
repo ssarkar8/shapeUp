b98c86ed9d15512c865ea7759cabb284252be532
zwachtel50 <zwachtel50@gmail.com>
2019-12-06 15:34:36 -0600

    added shake

--- app/src/main/java/com/example/shapeup/MainActivity.java
+++ app/src/main/java/com/example/shapeup/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.shapeup;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
@@ -36,61 +39,49 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
     ImageView pic;
 
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_screen);
 
         pic = findViewById(R.id.image);
-        //from documentation
-
-
-        // Instantiate the RequestQueue.
-        //RequestQueue queue = Volley.newRequestQueue(this);
-        //String url = "https://dog.ceo/api/breeds/image/random";
-
-
-        // Request a string response from the provided URL.
-        //JsonParser().parse(resp).getAsJsonObject()
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+            public void onShake(int count) {
+                startDogAPI(pic);
+            }
+        });
 
         TextView textBox = findViewById(R.id.thing);
         textBox.setText("");
         Button messageButton = findViewById(R.id.message);
+        //when button is clicked, load new image
         messageButton.setOnClickListener(unused -> {
-
             textBox.setText("The button was pressed!");
-            startAPI(pic);
-            // AsyncTaskRunner runner = new AsyncTaskRunner();
-            //  runner.doInBackground();
-
+            startDogAPI(pic);
         });
     }
 
-    void startAPI(ImageView view) {
+    void startDogAPI(ImageView view) {
         view.setImageResource(0);
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = "https://dog.ceo/api/breeds/image/random";
         try {
-
             JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
                     new Response.Listener<JSONObject>() {
                         @Override
                         public void onResponse(JSONObject response) {
                             Log.d("My App", "success!!!!!!");
-
-
-
                             try {
-
                                 String newUrl = response.get("message").toString();
-
-
                                 Picasso.get().load(newUrl).tag("current").into(view);
-                                //textBox.setText(",erp");
                                 Log.d("My App", "success!!!!!!");
-
-
-                                //Picasso.with(context).load(newUrl).into(pic);
                             } catch (JSONException e) {
                                 Log.e("My App", "json exception");
                             }
@@ -98,7 +89,6 @@ public class MainActivity extends AppCompatActivity {
                     }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-
                     Log.d("My App", "Sad" + error.toString());
                     return;
                 }
@@ -107,81 +97,27 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             System.out.println("uh oh");
         }
-
+    }
+    void startQuoteApi() {
 
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        // Add the following line to unregister the Sensor Manager onPause
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
+    }
+
 }
-/*
-    private class AsyncTaskRunner extends AsyncTask<String, String, String> {
-        //private String url;
-        //private String resp;
-        //ProgressDialog progressDialog;
 
-        //@Override
-        protected void doInBackground() {
-            String url = "https://dog.ceo/api/breeds/image/random";
-            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                    new Response.Listener<String>() {
-
-                        public void onResponse(String response) {
-                            Log.d("My App", "success!!!!!!");
-
-                            String resp = response;
-
-                            try {
-                                JSONObject obj = new JSONObject(resp);
-                                String newUrl = obj.getString("message");
-
-                                URL bla = new URL(newUrl);
-                                Bitmap image = BitmapFactory.decodeStream(bla.openConnection().getInputStream());
-                                pic.setImageBitmap(image);
-                                //textBox.setText(",erp");
-                                Log.d("My App", "success!!!!!!");
-
-                                //Picasso.with(context).load(newUrl).into(pic);
-                            } catch (JSONException e) {
-                                Log.e("My App", "json exception");
-                            } catch (IOException e) {
-                                Log.e("My App", "IO exception");
-                            }
-                        }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-
-                    Log.d("My App", "Sad" + error.toString());
-                    return;
-                }
-            });
-
-        }
-
-
-
-        //@Override
-        protected void onPostExecute(String result) {
-            // execution of result of Long time consuming operation
-            //progressDialog.dismiss();
-            //finalResult.setText(result);
-        }
-
-
-        @Override
-        protected void onPreExecute() {
-            //progressDialog = ProgressDialog.show(MainActivity.this,
-                    //"ProgressDialog",
-                    //"Wait for "+time.getText().toString()+ " seconds");
-        }
-
-
-        //@Override
-        protected void onProgressUpdate(String... text) {
-            //finalResult.setText(text[0]);
-
-        }
-    }
-
- */
 
 
 
